1ac467dca70f07421ca8cb5e6ade9604a664b5cc
NoeMM <mm.noe2011@gmail.com>
2025-06-07 20:55:55 +0200

    pipeline CI e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/#',
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    video:true,
+    screenshotOnRunFailure:true
   },
-  
+
 });
